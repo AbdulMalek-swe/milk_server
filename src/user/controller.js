efa43cf7e0ed9f541d3bbac4556eea25f966acad
AbdulMalek-swe <abdulmalek.swe.585@gmail.com>
2024-04-05 23:33:58 +0600

    fix:model

--- src/user/controller.js
+++ src/user/controller.js
@@ -1,18 +1,25 @@
+const { signupService } = require("./service");
+
  
  
    module.exports.signup = async (req, res) => {
-       try {
-        res.status(200).json({
-            message:"successfull",
-            result:{
-                token:"token for auth",
-                result:req.body,
-            }
-        })
-       } catch (error) {
+    try {
+        const user = await signupService(req.body);
+        //  const token =  await  user.generateConfirmationToken()
+        await user.save({validateBeforeSave:false});
+          console.log(user)
+       
+        res.status(200).json({  
+          status: "succesfful",
+          message: "account create successfully",
+          result:user
+        });
+      }
+      catch (error) {
         res.status(401).json({
-            error:error.message
+         error: error.message
         })
-       }
-   } 
+      } 
+    } 
+    
    
